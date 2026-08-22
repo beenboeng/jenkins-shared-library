01e0d0fa1e95f05c98e4e38720d73b183a20cede
been.boeng <been.boeng42@gmail.com>
2026-08-22 11:20:06 +0700

    Refactor SonarQube scanning process to use Docker and improve configuration handling

--- vars/scanSonarqube.groovy
+++ vars/scanSonarqube.groovy
@@ -1,28 +1,45 @@
 def call(Map config = [:]) {
 
-    String sonarqubeToken = config.get('sonarqubeToken', 'SONARQUBE-TOKEN')
-    String sonarScannerInstallation = config.get('sonarScannerInstallation', 'sonar-scanner')
-    String sonarServerInstallation = config.get('sonarServerInstallation', sonarScannerInstallation)
-    String projectName = config.projectName
-    String projectVersion = config.get('projectVersion', '1.0.0')
-    String projectKey = config.projectKey
-
-    if (!projectName) {
-        error "scanSonarqube: projectName is required"
-    }
+    String sonarServerInstallation = config.get('sonarServerInstallation', 'SonarQube')
+    String sonarqubeToken          = config.get('sonarqubeToken', 'sonar-token')
+    String scannerImage            = config.get('scannerImage', 'sonarsource/sonar-scanner-cli')
+    String projectKey              = config.projectKey
+    String projectName             = config.get('projectName', projectKey)
+    String sources                 = config.get('sources', '.')
 
     if (!projectKey) {
         error "scanSonarqube: projectKey is required"
     }
 
-    String scannerHome = tool(sonarScannerInstallation)
+    withSonarQubeEnv(sonarServerInstallation) {
+        withCredentials([
+            string(
+                credentialsId: sonarqubeToken,
+                variable: 'SONAR_AUTH_TOKEN'
+            )
+        ]) {
+            withEnv([
+                "SONAR_SCANNER_IMAGE=${scannerImage}",
+                "SONAR_PROJECT_KEY=${projectKey}",
+                "SONAR_PROJECT_NAME=${projectName}",
+                "SONAR_SOURCES=${sources}"
+            ]) {
+                sh '''
+                    echo "Running SonarQube analysis..."
 
-    withSonarQubeEnv(credentialsId: sonarqubeToken, installationName: sonarServerInstallation) {
-        sh """
-            ${scannerHome}/bin/sonar-scanner \
-                -Dsonar.projectName=${projectName} \
-                -Dsonar.projectKey=${projectKey} \
-                -Dsonar.projectVersion=${projectVersion}
-        """
+                    docker run --rm \
+                        -v "$WORKSPACE:/usr/src" \
+                        -w /usr/src \
+                        -e SONAR_HOST_URL="$SONAR_HOST_URL" \
+                        -e SONAR_AUTH_TOKEN="$SONAR_AUTH_TOKEN" \
+                        "$SONAR_SCANNER_IMAGE" \
+                        -Dsonar.projectKey="$SONAR_PROJECT_KEY" \
+                        -Dsonar.projectName="$SONAR_PROJECT_NAME" \
+                        -Dsonar.sources="$SONAR_SOURCES" \
+                        -Dsonar.host.url="$SONAR_HOST_URL" \
+                        -Dsonar.token="$SONAR_AUTH_TOKEN"
+                '''
+            }
+        }
     }
 }
